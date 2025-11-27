63229d08b3b38a23e87b3f3f094e92b573482116
Libolga <108710219+Libolga@users.noreply.github.com>
2025-11-28 00:12:56 +1000

    Create BlockExplorer.jsx

---
+++ src/components/BlockExplorer.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import Block from "./Block";
+
+function BlockExplorer({provider}) {
+  const [blockNumber, setBlockNumber] = useState(0);
+
+  useEffect(() => {
+    async function getBlockNumber() {
+      const latestBlockNum = await provider.getBlockNumber();
+      console.log("latest block: ", latestBlockNum);
+      setBlockNumber(latestBlockNum);
+    }
+    getBlockNumber();
+  });
+
+  return (
+    <div className="container">
+      <h2>Block Explorer</h2>
+      <p>Latest Block: {blockNumber}</p>
+      <Block blocknum={blockNumber} provider={provider} />
+      <Block blocknum={blockNumber - 1} provider={provider} />
+      <Block blocknum={blockNumber - 2} provider={provider} />
+    </div>
+  )
+}
+
+export default BlockExplorer;
